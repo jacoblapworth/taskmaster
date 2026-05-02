c9ae3448521a9a16fc7f2f19249cf879a92e9f86
J <jacob@lapworth.nz>
2026-05-02 19:15:00 +0100

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,13 @@
+import { defineMain } from "@storybook/nextjs-vite/node"
+
+export default defineMain({
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+  ],
+  framework: "@storybook/nextjs-vite",
+  staticDirs: ["../public"],
+})
---
+++ .storybook/preview.ts
@@ -0,0 +1,32 @@
+import addonDocs from "@storybook/addon-docs"
+import addonThemes, { withThemeByClassName } from "@storybook/addon-themes"
+import { definePreview } from "@storybook/nextjs-vite"
+
+export default definePreview({
+  addons: [addonDocs(), addonThemes()],
+
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      defaultTheme: "dark",
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+    }),
+  ],
+})
